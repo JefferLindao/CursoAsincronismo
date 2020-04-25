8754ef2eed1c0a8318f333f5bf19538d212bea4b
JefferLindao <jefferlindao@gmail.com>
2020-04-25 14:33:44 -0500

    Clase6 - Conociendo Async await

---
+++ src/async/index.js
@@ -0,0 +1,32 @@
+/**
+ * Implemantando Async Awai
+ */
+
+const doSomethingAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true) ?
+        setTimeout(() => resolve('Do Something Async'), 3000): reject(new Error('Test Error'))
+    })
+}
+
+const doSomething = async() => {
+    const something = await doSomethingAsync()
+    console.log(something)
+}
+
+console.log('Before')
+doSomething();
+console.log('After')
+
+const anotherFunction = async() => {
+    try {
+        const something = await doSomethingAsync()
+        console.log(something)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+console.log('Before')
+anotherFunction();
+console.log('After')
